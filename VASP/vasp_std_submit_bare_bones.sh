4c4425679d4468a115d231dd2c00902114c1fcec
John Whiting <john.whiting@nesi.org.nz>
2025-05-20 13:55:53 +1200

    Update vasp_std_submit_bare_bones.sh

--- VASP/vasp_std_submit_bare_bones.sh
+++ VASP/vasp_std_submit_bare_bones.sh
@@ -26,21 +26,26 @@ srun --job-name=print_binding_stats bash -c "echo -e \"Task #\${SLURM_PROCID} is
 echo -e "\n====== Finished printing CPU binding information, now launching VASP ======\n"
 srun -K1 vasp_std
 
-
-### ================ Notes on the hardware configuration of Genoa nodes ================ ###
-
-## Genoa nodes are partitioned into 8 NUMA domains. You can verify the number of NUMA domains by running "srun --partition=genoa2 bash -c 'numactl -H'"
+### ================ Notes on the hardware configuration of NeSI compute nodes ================ ###
+## Genoa
+## Genoa nodes are partitioned into 8 NUMA domains. You can verify the number of NUMA domains by running "srun --partition=genoa numactl -H"
 ## Each NUMA domain has 3 CCDs, 3 memory controllers, and 1 I/O hub.
 ## A Genoa node has:
 ##   8 NUMA domains
-##   1 Slurm socket-per-NUMA-domain
-##   21 physical cores-per-socket.
+##   21 physical cores-per-Slurm socket.
 
-## We want all cpus-per-task (i.e., threads of a rank) to share a NUMA domain as this improves data locality between CPUs. This is critically important given optimization (and related FFTs) of a particular orbital is dominated by floating point operations that require quick access to data stored in cache/RAM.
+## Milan
+## Milan nodes are partitioned into 8 NUMA domains. You can verify the number of NUMA domains by running "srun --partition=milan numactl -H"
+## Each NUMA domain has 3 CCDs, 3 memory controllers, and 1 I/O hub.
+## A Milan node has:
+##   8 NUMA domains
+##   16 physical cores-per-Slurm socket.
+
+## We want all cpus-per-task (i.e., threads of a rank) to share a NUMA domain as this improves data locality between CPUs. This is critically important given optimization (and related FFTs) of a particular orbital are dominated by floating point operations which require quick access to data stored in cache/RAM.
 ## Data locality settings:
-## --extra-node-info=1:*:*      To ensure your job does not share a socket with other jobs we restrict node selection to nodes with at least 1 socket that has all (*) cores and threads available.
-## --distribution=*:block:*     Bind tasks to CPUs on the same socket, and fill that socket before moving to the next consecutive socket. Multiple tasks will share a socket as long as cpus-per-task*ntasks < 21 is satisfied. 
-## --threads-per-core=1         Disable hyperthreding. In other words, bind the threads to the physical cores.
+## --extra-node-info=1:*:*      To ensure your job does not share a Slurm socket with other jobs we restrict node selection to nodes with at least 1 socket that has all (*) cores and threads available.
+## --distribution=*:block:*     Bind tasks to CPUs on the same Slurm socket, and fill that socket before moving to the next consecutive socket. Multiple tasks will share a socket as long as cpus-per-task*ntasks < physical cores-per-Slurm socket.
+## --threads-per-core=1         Disable hyperthreding. In other words, don't let cores appear to have two CPUs.
 ## --mem-bind=local             Use memory local to the processor in use. The OS should do this anyway but does not hurt to include.
 
 ### ==================================================================================== ###
